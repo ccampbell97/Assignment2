44dc90f5b636a1e27ace5da714e23ca69fa55663
ccampbell97 <47377048+ccampbell97@users.noreply.github.com>
2019-03-19 21:44:12 -0700

    Update person.h

--- person.h
+++ person.h
@@ -13,6 +13,7 @@ private:
 	float  hoursWorked;
 public:
 	Person();
+	Person(string firstName, string lastName, float payRate, float hoursWorked);
 	void setLastName(string lName);
 	string getLastName();
 	void setFirstName(string fName);
@@ -25,4 +26,3 @@ public:
 	string fullName();
 };
 #endif // end person.h
-
